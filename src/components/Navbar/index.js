2f9ed2c8a74b5136831ca70bfaaff6194ded4acd
Joshua Bradley <bradleyjd19@gmail.com>
2019-07-06 17:29:15 -0500

    made app more responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import "./style.css";
 
 function Navbar(props) {
-    return (
-      <div>
-        <div id="navbar">
-          <span id="gameTitle">The Letterkenny Click Game</span>
-          <span id="message">{props.message}</span>
-          <span id="topScore">Top Score: {props.topScore}</span>
-          <span id="divider">|</span>
+  return (
+    <div>
+      <div id="navbar">
+        <span id="gameTitle">The Letterkenny Click Game</span>
+        <span id="message">{props.message}</span>
+        <span id="scoreDiv">
           <span id="currentScore">Current Score: {props.currentScore}</span>
-        </div>
+          <span id="divider">|</span>
+          <span id="topScore">Top Score: {props.topScore}</span>
+        </span>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Navbar;
